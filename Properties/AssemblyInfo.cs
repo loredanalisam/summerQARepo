132a5e3c90ed9f029c9726851f36e50bf223d159
loredanai <loredana.iuhas@lisam.com>
2020-07-28 12:41:09 +0200

    initial project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SummerQA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SummerQA")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25a38e02-d0b7-4b3b-9122-cdf6c2f3a819")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SummerQA
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GoToUrl()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.shopmania.ro/");
+            driver.Manage().Window.Maximize();
+            IWebElement searchBox = driver.FindElement(By.Id("autocomplete_prod"));
+
+            searchBox.SendKeys("telefon");
+            searchBox.SendKeys(Keys.Enter);
+            Thread.Sleep(4000);
+
+            Assert.IsTrue(driver.FindElementByClassName("label").Displayed);
+        }
+
+    }
+}
